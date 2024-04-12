703aa45bb45e7be05b15b1c507ab31dcd0db0b70
alpeme14w2 <110390625+alpeme14w2@users.noreply.github.com>
2024-04-13 06:59:11 +0700

    asdsasd

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
 	name: "The ??? Tree",
 	id: "mymod",
 	author: "nobody",
-	pointsName: "points",
+	pointsName: "Resettime",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
